9ac7a80dcac0effd57af74af6acc13d580001ad9
Pierre HERMAN <pierre.herman@yxir.tech>
2023-09-20 11:49:34 -0400

    Added react project + updated docker compose

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className={'h-full bg-gray-50'}>
+            <div className={'flex justify-center'}>
+                <h2 className={'text-4xl font-bold'}>GoInventory™</h2>
+            </div>
+        </div>
+    );
+}
+
+export default App;
