1e368deb9c02737aa9d0b7a5bb9b4207b67dc7cc
Pierre HERMAN <pierre.herman@yxir.tech>
2023-09-22 01:02:03 -0400

    Added notif component + notif computing

--- frontend/src/stores/products.store.ts
+++ frontend/src/stores/products.store.ts
@@ -7,25 +7,36 @@ import {getAllCategories} from "../services/categories.service";
 interface GoInventoryStore {
     products: Product[];
     categories: Category[];
+    notifications: string[];
     selectedCategoryId: number | undefined;
     setSelectedCategoryId: (category: number | undefined) => void,
-    setProducts: (newProducts: Product[]) => void;
     fillProducts: () => Promise<void>;
     fillCategories: () => Promise<void>;
     deleteProductFromStore: (productId: number) =>  void;
 }
 
+const generateNotifications = (newProducts: Product[], existingNotifications: string[]): string[] => {
+    const lowQuantityProducts = newProducts.filter((product) => product.quantity < 5);
+    const notifications = lowQuantityProducts.map(
+        (product) => `Le produit "${product.name}" a une quantité inférieure à 5.`
+    );
+    return notifications;
+}
+
 const useGoInventoryStore = create<GoInventoryStore>((set, get) => ({
     products: [],
     categories: [],
+    notifications: [],
     selectedCategoryId: undefined,
     setSelectedCategoryId: (category) => set({ selectedCategoryId: category }),
-    setProducts: (newProducts) => set({ products: newProducts }),
     fillProducts: async () => {
         const categoryId = get().selectedCategoryId
         try {
             const products = await getProducts({categoryId});
             set({ products });
+            if (categoryId) return;
+            const notifications = generateNotifications(products, get().notifications)
+            set({notifications})
         } catch (error) {
             console.error('Une erreur s\'est produite lors du remplissage des produits dans le store :', error);
         }
